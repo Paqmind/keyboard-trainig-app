0b7d3a2b76d8a86192a5eb7cf7c289d2f522c2bb
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2017-12-11 00:02:47 +0300

    radio buttons styles changed

--- src/components/OptionalBar.js
+++ src/components/OptionalBar.js
@@ -2,12 +2,14 @@ import React from "react"
 
 const OptionalBar = (props) => {
   return <div className="optional-bar" id="mode">
-    <div className="radio-buttons">
-      <input type="radio" id="beginner" name="mode" value="beginner" onClick={props.handler} defaultChecked={true} />
-      <label htmlFor="beginner">beginner</label>
-      <input type="radio" id="advanced" name="mode" value="advanced" onClick={props.handler} />
-      <label htmlFor="advanced">advanced</label>
-    </div>
+    <label className="container">Beginner
+      <input type="radio" name="radio" value="beginner" onClick={props.handler} defaultChecked={true}/>
+      <span className="checkmark"></span>
+    </label>
+    <label className="container">Advanced
+      <input type="radio" name="radio" value="advanced" onClick={props.handler}/>
+      <span className="checkmark"></span>
+    </label>
   </div>
 }
 
