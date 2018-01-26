ee3a5b6ee031ff8c716b4c7c935b3f7d531af70d
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-01-26 22:42:18 +0300

    buttons classnames generator helper added

---
+++ src/components/btnClassnamesGenerator.js
@@ -0,0 +1,11 @@
+const classNames = require('classnames')
+
+export default function classNamesGenerator(props, obj) {
+ return classNames({
+   "key": true,
+   [`${obj.caption}`]: true,
+   [`${obj.code}`]: true,
+   "key-highlighted": props.btnHighlighted == obj.code,
+   "key-pressed": props.btnPressed == obj.code
+ })
+}
