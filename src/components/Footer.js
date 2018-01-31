e3a5b4c05e93f4649bfce9c25c8727b35ccb7ffc
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-01-31 21:43:02 +0300

    links changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return <div className="footer">
-    <span><a href="https://github.com/datexgo/keyboard-trainig-app">GitHub repo</a></span>
-    <span><a href="https://datexgo.github.io/resume/">Author</a></span>
-    <span><a href="http://klava.org/">Original</a></span>
+    <span><a href="https://github.com/datexgo/keyboard-trainig-app">github</a></span>
+    <span><a href="https://datexgo.github.io/resume/">author</a></span>
+    <span><a href="http://klava.org/">inspired by</a></span>
   </div>
 }
 
