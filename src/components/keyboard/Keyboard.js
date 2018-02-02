c8434150974b611f80e8c998b4cd17654b74d83f
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-02-02 09:59:28 +0300

    keyboard jsx elements changed

--- src/components/keyboard/Keyboard.js
+++ src/components/keyboard/Keyboard.js
@@ -5,28 +5,28 @@ import classNamesGenerator from './classNames'
 const Keyboard = props => {
   return <div id="keyboard">
     <ul className="cf" id="numbers">
-      <li><a href="#" className="key tilda 192"><b>~</b><span>`</span></a></li>
-      <li><a href="#" className="key one 49"><b>!</b><span>1</span></a></li>
-      <li><a href="#" className="key two 50"><b>@</b><span>2</span></a></li>
-      <li><a href="#" className="key three 51"><b>#</b><span>3</span></a></li>
-      <li><a href="#" className="key four 52"><b>$</b><span>4</span></a></li>
-      <li><a href="#" className="key five 53"><b>%</b><span>5</span></a></li>
-      <li><a href="#" className="key six 54"><b>^</b><span>6</span></a></li>
-      <li><a href="#" className="key seven 55"><b>&amp;</b><span>7</span></a></li>
-      <li><a href="#" className="key eight 56"><b>*</b><span>8</span></a></li>
-      <li><a href="#" className="key nine 57"><b>(</b><span>9</span></a></li>
-      <li><a href="#" className="key zero 48"><b>)</b><span>0</span></a></li>
-      <li><a href="#" className="key minus 189 alt"><b>_</b><span>-</span></a></li>
-      <li><a href="#" className="key equals 187"><b>+</b><span>=</span></a></li>
-      <li><a href="#" className="key delete 46" id="delete"><span>Delete</span></a></li>
+      <li><div className="key tilda 192"><b>~</b><span>`</span></div></li>
+      <li><div className="key one 49"><b>!</b><span>1</span></div></li>
+      <li><div className="key two 50"><b>@</b><span>2</span></div></li>
+      <li><div className="key three 51"><b>#</b><span>3</span></div></li>
+      <li><div className="key four 52"><b>$</b><span>4</span></div></li>
+      <li><div className="key five 53"><b>%</b><span>5</span></div></li>
+      <li><div className="key six 54"><b>^</b><span>6</span></div></li>
+      <li><div className="key seven 55"><b>&amp;</b><span>7</span></div></li>
+      <li><div className="key eight 56"><b>*</b><span>8</span></div></li>
+      <li><div className="key nine 57"><b>(</b><span>9</span></div></li>
+      <li><div className="key zero 48"><b>)</b><span>0</span></div></li>
+      <li><div className="key minus 189 alt"><b>_</b><span>-</span></div></li>
+      <li><div className="key equals 187"><b>+</b><span>=</span></div></li>
+      <li><div className="key delete 46" id="delete"><span>Delete</span></div></li>
     </ul>
     <ul className="cf" id="qwerty">
       {
         keys[0].map((obj) => {
           return <li key={obj.code}>
-            <a href="#" className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
+            <div className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
               <span>{obj.caption}</span>
-            </a>
+            </div>
           </li>
         })
       }
@@ -35,9 +35,9 @@ const Keyboard = props => {
       {
         keys[1].map((obj) => {
           return <li key={obj.code}>
-            <a href="#" className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
+            <div className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
               <span>{obj.caption}</span>
-            </a>
+            </div>
           </li>
         })
       }
@@ -46,9 +46,9 @@ const Keyboard = props => {
       {
         keys[2].map((obj) => {
           return <li key={obj.code}>
-            <a href="#" className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
+            <div className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
               <span>{obj.caption}</span>
-            </a>
+            </div>
           </li>
         })
       }
@@ -57,19 +57,19 @@ const Keyboard = props => {
       {
         keys[3].map((obj) => {
           return <li key={obj.key}>
-            <a href="#" className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
+            <div className={classNamesGenerator(props.state, obj)} id={obj.id || null}>
               <span>{obj.caption}</span>
-            </a>
+            </div>
           </li>
         })
       }
       <ol className="cf">
-        <li><a href="#" className="key arrow 37" id="left"><span>&#9668;</span></a></li>
+        <li><div className="key arrow 37" id="left"><span>&#9668;</span></div></li>
         <li>
-          <a href="#" className="key arrow 38" id="up"><span>&#9650;</span></a>
-          <a href="#" className="key arrow 40" id="down"><span>&#9660;</span></a>
+          <div className="key arrow 38" id="up"><span>&#9650;</span></div>
+          <div className="key arrow 40" id="down"><span>&#9660;</span></div>
         </li>
-        <li><a href="#" className="key arrow 39" id="right"><span>&#9658;</span></a></li>
+        <li><div className="key arrow 39" id="right"><span>&#9658;</span></div></li>
       </ol>
     </ul>
   </div>
