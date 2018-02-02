792c1db48774c360d0ac1cf3c4448a49bb58e756
Artem Kozyrev <artem.kozyrev.dev@gmail.com>
2018-02-02 10:09:03 +0300

    footer links color canged

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return <div className="footer">
-    <span><a href="https://github.com/datexgo/keyboard-trainig-app">github</a></span>
-    <span><a href="https://datexgo.github.io/resume/">author</a></span>
-    <span><a href="http://klava.org/">inspired by</a></span>
+    <span><a href="https://github.com/datexgo/keyboard-trainig-app" className="link">github</a></span>
+    <span><a href="https://datexgo.github.io/resume/" className="link">author</a></span>
+    <span><a href="http://klava.org/" className="link">inspired by</a></span>
   </div>
 }
 
